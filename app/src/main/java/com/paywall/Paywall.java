e0fa89592f04c18de53fc70e9ec66432ee3c3f1f
alon <alon.ponch@gmail.com>
2026-02-13 16:26:45 +0200

    Move the token from the file

--- app/src/main/java/com/paywall/Paywall.java
+++ app/src/main/java/com/paywall/Paywall.java
@@ -72,19 +72,7 @@ public class Paywall implements Runnable {
 
     public static void check_paywall() throws IOException, JSONException {
         Log.i(TAG, "Checking for paywall...");
-        AssetManager assetManager = Objects.requireNonNull(Utils.getApplication()).getAssets();
-        InputStream file = assetManager.open("paywall.json");
-        String file_content;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            file_content = new String(file.readAllBytes());
-        } else {
-            int size = file.available();
-            byte[] buffer = new byte[size];
-            file.read(buffer);
-            file_content = new String(buffer);
-        }
-        JSONObject json = new JSONObject(file_content);
-        String token = json.getString("token");
+        String token = "{{PAYWALL_TOKEN}}";
         if (!is_customer_subscribed(token)) {
             throw new RuntimeException("Customer is not subscribed");
         }
